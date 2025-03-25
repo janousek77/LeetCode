c1b71e33ebe9c185cd05d4a18c8107f3943c56da
janousek <janousek77@gmail.com>
2025-03-25 13:32:57 -0700

    remove_element done

---
+++ Python/easy/remove_element.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def removeElement(nums: List[int], val: int) -> int:
+    i = 0
+    while i < len(nums):
+        if nums[i] == val:
+            nums.pop(i)
+        else:
+            i += 1
+    
+    print(nums)
+    return len(nums)
+
+print(removeElement([3,2,2,3], 3))
+print(removeElement([0,1,2,2,3,0,4,2], 2))
